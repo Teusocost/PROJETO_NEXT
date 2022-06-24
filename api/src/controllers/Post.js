774f2f06388d3a4404daa84d876e8e79a73d917f
NathaN-CNC <nathan.c.smv@gmail.com>
2022-06-24 09:22:53 -0300

    projeto3 criada a funcionalidade de buscar os posts criados pelos usuários

--- api/src/controllers/Post.js
+++ api/src/controllers/Post.js
@@ -2,9 +2,11 @@ const PostModel = require('../models/Post')
 
 const Post = {
     async createPost(req, res) {
-        const { name, company_name, description, symbol, currency, price, userId } = req.body
+        const { name, company_name, description, currency, price, userId } = req.body
+        let { symbol } = req.body
 
         if (name && company_name && description && symbol && currency && price && userId) {
+            symbol = symbol.toUpperCase()
             const post = await PostModel.find({ symbol: symbol })
 
             if (post.length > 0) {
@@ -19,9 +21,20 @@ const Post = {
     },
 
     async readPosts(req, res) {
-        const { symbol } = req.query
+        let { symbol } = req.query
 
-        res.status(400).json({ code: 400 })
+        if(symbol) {
+            symbol = symbol.toUpperCase()
+
+            const post = await PostModel.find({ symbol: symbol })
+
+            let results = {}
+            results[symbol] = post[0] || { error: true, message: "Não foi possível encontrar nenhuma ação com o símbolo buscado" }
+
+            return res.status(200).json({ code: 200, message: 'ok', results: results, by: 'symbol' })
+        }
+
+        res.status(400).json({ code: 400, message: 'É obrigatório o envio de um símbolo'})
     }
 }
 
