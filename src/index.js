c781ebf6e61f36d24173cce460e6b0bc3af90cdb
NathaN-CNC <nathan.c.smv@gmail.com>
2022-06-24 10:32:12 -0300

    commit na heroku

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const db = require('./database/config')
 var { expressjwt: jwt_middleware } = require("express-jwt");
 const JWT_CONFIG = { secret: process.env.SECRET_JWT, algorithms: ["HS256"] }
 
-const port = process.env.PORT || 4100
-
 // Controllers
 const UserController = require('./controllers/User')
 const PostController = require('./controllers/Post')
@@ -28,6 +26,7 @@ app.post('/api/posts', jwt_middleware(JWT_CONFIG), PostController.createPost)
 app.get('/api/posts', jwt_middleware(JWT_CONFIG), PostController.readPosts)
 
 // Start the server
+const port = process.env.PORT || 4100
 app.listen(port, () => console.log('> The server is running on port: ' + port + '\n'))
 
 // Connect to the database
